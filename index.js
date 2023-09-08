bb410b54ff767afdf80330921bc3b652f575825d
3142857143 <76162747+3142857143@users.noreply.github.com>
2023-09-08 10:04:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
+
 search.addEventListener('click', () => {
 
     const APIKey = '9e671757e155fa388e7ede0fd8812679';
